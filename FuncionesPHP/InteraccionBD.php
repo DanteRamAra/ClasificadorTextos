105b98bf4d090d11c58ac6067e56c8061c9d5501
DanteRamirezAranda <darthdant@gmail.com>
2025-05-27 14:23:28 -0600

    VALIDACIONES REGIISTRO 80% BACK

--- FuncionesPHP/InteraccionBD.php
+++ FuncionesPHP/InteraccionBD.php
@@ -54,35 +54,3 @@ class publicaciones{
     }
 }
 
-class validaciones{
-    public static function encontrarEmail($email){ //existencia de email
-        try {
-            $conn=new PDO("mysql:host=localhost;port=3306;dbname=adsclasificador",'root','');
-            $consult=$conn->prepare("select correo_usuario from usuarios where correo_usuario=:email ");
-            $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            $consult->execute([':email'=>$email]);
-            $resultado=$consult->fetchAll();
-            return $resultado;
-        } catch (PDOException $error) {
-            echo "conexion erronea".$error->getMessage();
-            
-        }
-    }
-    public static function validarInicio($datos){
-       try {
-            $conn=new PDO("mysql:host=localhost;port=3306;dbname=adsclasificador",'root','');
-            $consult=$conn->prepare("select id_usuario from usuarios where correo_usuario=:correo_usu && password_usuario=:pass_usuario");
-            $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            $consult->execute(array(
-            ':correo_usu'=>$datos[0],
-            ':pass_usuario'=>$datos[1]  
-            ));
-            $resultado = $consult->fetch(PDO::FETCH_ASSOC);
-            return $resultado ? $resultado['id_usuario'] : false;
-        } catch (PDOException $error) {
-            echo "conexion erronea".$error->getMessage();
-        }  
-    }
-}
-
-?>
--- register.php
+++ register.php
@@ -1,36 +1,56 @@
 <?php
     session_start();
-    require ('FuncionesPHP/funciones.php');
+    require ('FuncionesPHP/InteraccionBD.php');
+    require ('FuncionesPHP/validacionesBack.php');
     require('funcion.php');
     verificarSesionActiva();//verifica si existe una sesion iniciada en ese momento
     if($_POST){
-        $contra=password_hash($_POST['pass'], PASSWORD_DEFAULT);//cifra contraseña
-         if (strlen($_POST['pass']) < 8) {//validacion de numero caracteres
+      //limpieza de datos en caso de tener espacios al inicio o final
+      $nombreLimpio=trim($_POST['name']);
+      $appLimpio=trim($_POST['apm']);
+      $apmLimpio=trim($_POST['apm']);
+      $emailLimpio=trim($_POST['name']);
+      $passLimpio=trim($_POST['name']);
+
+      //validaciones finales 
+      $passValida=strlen($_POST['pass'])>=8;
+      $emailNoExistente=!validaciones::encontrarEmail($emailLimpio);
+      $emailValido=filter_var($emailLimpio,FILTER_VALIDATE_EMAIL);
+
+      if(strlen($_POST['pass'])>=8){//validaciones back
         
-          die("Error: La contraseña debe tener al menos 8 caracteres.");
-        } 
-        $datos=[$name=$_POST['name'],
-        $app=$_POST['app'],
-        $apm=$_POST['apm'],
-        $fechaNac=$_POST['fechaNac'],
-        $email=$_POST['email'],
-        $contra];
-        $verificaEmail=validaciones::encontrarEmail($email);
-        if(!($email=$verificaEmail)){//verifica que el email no exista
+        if(!($_POST['email']==$verificaEmail)){//verifica que no exista el email
+
+          $contra=password_hash($_POST['pass'], PASSWORD_DEFAULT);//cifra contraseña
+          //arreglo para la BD
+          $datos=[$name=$_POST['name'],
+          $app=$_POST['app'],
+          $apm=$_POST['apm'],
+          $fechaNac=$_POST['fechaNac'],
+          $email=$_POST['email'],
+          $contra];
+
           $id_usuario = usuario::register($datos); //registra al usuario y saca el id
-          if ($id_usuario) {//redirige a inicio.php
-            session_start();
-            $_SESSION['id_usuario'] = $id_usuario;
-            header('location:inicio.php');
-            exit();
-          } else {
+
+        }
+       
+
+      }else{
+        die("Error: Datos ingresados incorrectamente");
+      }
+
+      
+      if($id_usuario){//redirige a inicio.php
+          session_start();
+          $_SESSION['id_usuario']=$id_usuario;
+          header('location:inicio.php');
+          exit();
+          }else{
             echo "Hubo un problema al registrar el usuario.";
           }
         }else{
           echo "Hubo un problema al registrar el usuario.";
-        }
-    }
-
+      }
     /*
     falta validaciones front de todo, back de todo menos correo y pass, numero de intentos
     */
@@ -80,7 +100,7 @@
         <br>
         <input type="text" name="apm" id="" placeholder="Apellido materno">
         <br>
-        <input type="date" name="fechaNac" id="">
+        <input type="date" name="fechaNac" id="" >
         <br>
         <input type="email" name="email" id="" placeholder="Email">
         <br>
